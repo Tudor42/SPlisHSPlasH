11a3b4906d8b17bea5f15f47283f9e465b6c236c
Tudor42 <puscas.tudor42@gmail.com>
2024-08-17 14:34:23 +0300

    timestep example

--- pySPlisHSPlasH/examples/animated_body.py
+++ pySPlisHSPlasH/examples/animated_body.py
@@ -3,39 +3,60 @@ import pysplishsplash.Utilities.SceneLoaderStructs as Scenes
 import numpy as np
 import math
 from scipy.spatial.transform import Rotation as R
+import os
+import tkinter as tk
+from tkinter import filedialog
+import sys 
+
+velocity = [0, 0, 0]
 
 def time_step_callback():  
     sim = sph.Simulation.getCurrent()
     boundary = sim.getBoundaryModel(1)
     animatedBody = boundary.getRigidBodyObject()
-   
-    animatedBody.setAngularVelocity([0, 60.0/180.0*math.pi, 0])
+    animatedBody.setVelocity([animatedBody.getVelocity()[i] + velocity[i] for i in range(3)])
     animatedBody.animate()
+    
+    #animatedBody.setAngularVelocity([0, 60.0/180.0*math.pi, 0])
+    #animatedBody.animate()
+
+def i():
+    global velocity
+    velocity = [0.1, 0, 0]
+    print("velocity set to", velocity)
+
+def k():
+    global velocity
+    velocity = [-0.1, 0, 0]
+    print("velocity set to", velocity)
+
+def j():
+    pass
+
+def l():
+    pass
 
 def main():
     # Set up the simulator
+    tk.Tk().withdraw()
+    scene_dir = os.path.join(os.path.dirname(sys.executable), "data/Scenes/")
+    scene_file = filedialog.askopenfilename(initialdir=scene_dir)
     base = sph.Exec.SimulatorBase()
-    base.init(useGui=True,  sceneFile=sph.Extras.Scenes.Empty)
-
+    base.init(useGui=True, useCache=False, sceneFile=scene_file)
+    #pbd = sph.PBD.PBDBoundarySimulator(base)
+    #base.setBoundarySimulator(pbd)
+    #base.deferredInit()
     # Create an imgui simulator
     gui = sph.GUI.Simulator_GUI_imgui(base)
     base.setGui(gui)
     base.setTimeStepCB(time_step_callback)
-
-    # Get the scene and add objects
-    scene = sph.Exec.SceneConfiguration.getCurrent().getScene()
-    
-    scene.boundaryModels.append(Scenes.BoundaryData(meshFile="../models/UnitBox.obj", translation=[0., 3.0, 0.], scale=[3., 6., 3.], color=[0.1, 0.4, 0.5, 1.0], isWall=True, mapInvert=True, mapResolution=[25, 25, 25]))
-    scene.boundaryModels.append(Scenes.BoundaryData(meshFile="../models/Dragon_50k.obj", translation=[0., 0.05, 0.], scale=[1,1,1], color=[0.5, 0.5, 0.5, 1.0], axis=[0,1,0], angle=30.0*math.pi/180.0, isAnimated=True, isWall=False, mapInvert=False, mapResolution=[25, 25, 25]))
-    
-    scene.fluidBlocks.append(Scenes.FluidBlock(id='Fluid', boxMin=[-1.25, 0.0, -1.25], boxMax = [-0.25, 1.5, -0.25], mode=0, initialVelocity=[0.0, 0.0, 0.0]))
-
-    # init the simulation
+    gui.addKeyFunc(73, 0 , i)
+    gui.addKeyFunc(75, 0 , k)
     base.initSimulation()
 
     sim = sph.Simulation.getCurrent()
-    sim.setValueInt(sim.BOUNDARY_HANDLING_METHOD, 2)
-    
+    sim.setValueInt(sim.BOUNDARY_HANDLING_METHOD, 0)
+
     base.runSimulation()
     base.cleanup()
 
--- pySPlisHSPlasH/examples/animated_body_2D.py
+++ pySPlisHSPlasH/examples/animated_body_2D.py
@@ -4,17 +4,34 @@ import numpy as np
 import math
 from scipy.spatial.transform import Rotation as R
 
+class MyTimeStep(sph.TimeStep):
+    def init(self):
+        print("INITIALIZED")
+
+    def step(self):
+        sim = sph.Simulation.getCurrent()
+        n = sim.numberOfFluidModels()
+        for i in range(n):
+            model = sim.getFluidModel(i)
+            for j in range(model.numActiveParticles()):
+                #print("Particle", j, "pos", model.getPosition(j))
+                model.setPosition(j, model.getPosition(j) + [0, 0.1, 0])
+        #sim.animateParticles();              
+
+    def resize(self):
+        print("resized")
+
+    def reset(self):
+        print("reset")
 
 def time_step_callback():  
     sim = sph.Simulation.getCurrent()
-    boundary = sim.getBoundaryModel(1)
-    animatedBody = boundary.getRigidBodyObject()
+    #boundary = sim.getBoundaryModel(1)
+    #animatedBody = boundary.getRigidBodyObject()
 
-    tm = sph.TimeManager.getCurrent()
-    t = tm.getTime()
-    #animatedBody.setAngularVelocity([0, 0, 10.0/180.0*math.pi])
-    animatedBody.setVelocity([ 0.35*math.sin(0.75*t), -0.35*math.cos(0.75*t), 0])
-    animatedBody.animate()
+    #tm = sph.TimeManager.getCurrent()
+    #t = tm.getTime()
+
 
 def main():
     # Set up the simulator
@@ -24,7 +41,7 @@ def main():
     # Create an imgui simulator
     gui = sph.GUI.Simulator_GUI_imgui(base)
     base.setGui(gui)
-    base.setTimeStepCB(time_step_callback)
+    #base.setTimeStepCB(time_step_callback)
 
     # Get the scene and add objects
     scene = sph.Exec.SceneConfiguration.getCurrent().getScene()
@@ -38,11 +55,11 @@ def main():
 
     scene.boundaryModels.append(Scenes.BoundaryData(meshFile="../models/UnitBox.obj", translation=[0., 3.0, 0.], scale=[3., 6., 3.], color=[0.1, 0.4, 0.5, 1.0], isWall=True, mapInvert=True, mapResolution=[25, 25, 25]))
 
-    scene.boundaryModels.append(Scenes.BoundaryData(meshFile="../models/Dragon_50k.obj", translation=[-0.45, 1, 0.], scale=[1.5,1.5,1.5], color=[0.5, 0.5, 0.5, 1.0], isAnimated=True, isWall=False, mapInvert=False, mapResolution=[25, 25, 25]))
+    #scene.boundaryModels.append(Scenes.BoundaryData(meshFile="../models/Dragon_50k.obj", translation=[-0.45, 1, 0.], scale=[1.5,1.5,1.5], color=[0.5, 0.5, 0.5, 1.0], isAnimated=True, isWall=False, mapInvert=False, mapResolution=[25, 25, 25]))
 
     scene.fluidBlocks.append(Scenes.FluidBlock(id='Fluid', boxMin = [-1.5, 0.0, -0.1], boxMax = [1.5 , 1.0, 0.1], mode=0, initialVelocity=[0.0, 0.0, 0.0]))
 
-    scene.emitters.append(Scenes.EmitterData(id='Fluid', x=[1,3,0], width=5, height=5, axis = [0,1,0], angle = math.pi, emitStartTime=0.0, emitEndTime=3.0, velocity=3))
+    #scene.emitters.append(Scenes.EmitterData(id='Fluid', x=[1,3,0], width=5, height=5, axis = [0,1,0], angle = math.pi, emitStartTime=0.0, emitEndTime=3.0, velocity=3))
 
     scene.materials.append(Scenes.MaterialData(id='Fluid', colorMapType=1, maxVal=8.0))
 
@@ -51,7 +68,8 @@ def main():
 
     sim = sph.Simulation.getCurrent()
     sim.setValueInt(sim.BOUNDARY_HANDLING_METHOD, 0)
-
+    timeStep = MyTimeStep()
+    sim.setTimeStep(timeStep)
     base.runSimulation()
     base.cleanup()
 
